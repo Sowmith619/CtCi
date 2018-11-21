83531314d40bb3a55eb016a043e4c74587d25a4b
SunilVasu <sunilkv24@gmail.com>
2018-11-20 22:36:13 -0600

    Array and String

--- src/CtCi/ArrayString.java
+++ src/CtCi/ArrayString.java
@@ -1,8 +1,10 @@
 package CtCi;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Arr_String_1 {
+public class ArrayString {
 	public static void main(String[] args) {
 		isUnique();
 		checkPermutation();
@@ -10,6 +12,8 @@ public class Arr_String_1 {
 		oneAway();
 		compression();
 		rotateMatrix();
+		spiralMatrix();
+		zeroMatrix();
 	}
 	//[1.1]Check isUnique: time O(n); space O(1) as the loop will not iterate over more than 128 character
 	public static void isUnique() {
@@ -136,25 +140,25 @@ public class Arr_String_1 {
 						{0,0,0}, 
 						{1,1,1}};
 		System.out.println("Rotate Matrix: Input matrix");
-		displayMat(mat);
+		displayMatrix(mat);
 		int n = mat.length;
 		for(int layer=0;layer<n/2;layer++) {
 			int first = layer;
 			int last = n-1-layer;
-			for(int i=first;i<last;i++) {
+			for(int i=first;i<=last;i++) {
 				int offset = i-first;
-				
-				int top = mat[first][i];
-				mat[first][i] = mat[last-offset][first];
+				int top = mat[i][first];
+				mat[i][first] = mat[last-offset][first];
 				mat[last-offset][first] = mat[last][last-offset];
-				mat[last][last-offset] = mat[i][last];
-				mat[i][last] = top;
+				mat[last][last-offset] = mat[last][i];
+				mat[last][i] = top;
 			}
 		}
 		System.out.println("Rotate Matrix: Using offset");
-		displayMat(mat);
+		displayMatrix(mat);
 		
-		//Method 2: Transpose and Flip
+		//Method 2:rotate 90 Transpose -> Flip
+		//rotate 180  Transpose -> Flip -> Transpose -> Flip
 		mat = new int[][] {{1,1,1}, {0,0,0}, {1,1,1}};
 		
 		for(int i=0;i<mat.length;i++) {
@@ -174,14 +178,74 @@ public class Arr_String_1 {
 		}
 		
 		System.out.println("Rotate Matrix: Using Transpose -> Flip ");
-		displayMat(mat);
+		displayMatrix(mat);
 	}
-	public static void displayMat(int[][] mat) {
+	public static void displayMatrix(int[][] mat) {
 		for(int i=0;i<mat.length;i++) {
 			for(int j=0;j<mat[0].length;j++) {
 				System.out.print(mat[i][j]+"  ");
 			}
 			System.out.print("\n");
 		}
+	}
+	//Matrix: convert a matrix elements into a list in spiral order
+	public static void spiralMatrix() {
+		int[][] mat = new int[][] {{1,1,1}, {0,0,0}, {1,1,1}};
+		
+		List<Integer> res = new LinkedList<>();
+		int top=0, bottom=mat.length-1;
+		int left=0, right=mat[0].length-1;
+		
+		while(top<=bottom && left<=right) {
+			for(int i=left;i<=right;i++)
+				res.add(mat[top][i]);
+			top++;
+			for(int i=top;i<=bottom;i++)
+				res.add(mat[i][right]);
+			right--;
+			if(top<=bottom) {
+				for(int i=right;i>=left;i--)
+					res.add(mat[bottom][i]);
+				bottom--;
+			}
+			if(right<=left) {
+				for(int i=bottom;i<=top;i++)
+					res.add(mat[i][top]);
+				right++;
+			}
+		}
+		System.out.println("Matrix Spiral:"+res);
+	}
+	//[1.8] ZeroMatrix
+	public static void zeroMatrix() {
+		int[][] mat = new int[][] {{1,1,1}, {1,0,1}, {1,1,1}};
+		boolean fr=false, fc=false;
+		
+		for(int i=0;i<mat.length;i++) {
+			for(int j=0;j<mat[0].length;j++) {
+				if(mat[i][j] == 0) {
+					if(i==0) fc=true;
+					if(j==0) fr=true;
+					mat[i][0] = 0;
+					mat[0][j] = 0;
+				}
+			}
+		}
+		for(int i=1;i<mat.length;i++) {
+			for(int j=1;j<mat[0].length;j++) {
+				if(mat[i][0]==0 || mat[0][j]==0)
+					mat[i][j]=0;
+			}
+		}
+		if(fr) {
+			for(int i=0;i<mat[0].length;i++)
+				mat[0][i]=0;
+		}
+		if(fc) {
+			for(int i=0;i<mat.length;i++)
+				mat[i][0]=0;
+		}
+		System.out.println("ZeroMatrix");
+		displayMatrix(mat);
 	}
 }
