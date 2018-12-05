bf66ff55f0cba38ca25cfb459eced3b08ac360bf
SunilVasu <sunilkv24@gmail.com>
2018-12-04 23:16:53 -0600

    ListNode remove duplicates

--- src/CtCi/LinkedList.java
+++ src/CtCi/LinkedList.java
@@ -1,5 +1,7 @@
 package CtCi;
 
+import java.util.HashSet;
+
 class ListNode{
 	int val;
 	ListNode next;
@@ -10,26 +12,48 @@ class ListNode{
 }
 
 public class LinkedList {
-	
-	public static void main(String[] args) {
-		ListNode root = init();
-		ListNode dummt = new ListNode(0);
-		
-		System.out.print("Vals :  ");
-		while(root!=null) {
-			System.out.print(root.val+"-");
-			root=root.next;
-		}
-	}
-	
 	public static ListNode init() {
 		ListNode temp = new ListNode(5);
 		temp.next = new ListNode(10);
 		temp.next.next = new ListNode(15);
-		temp.next.next.next = new ListNode(20);
-		
+		temp.next.next.next = new ListNode(15);
+		temp.next.next.next.next = new ListNode(20);
 		return temp;
 	}
+	public static void displayList(ListNode temp) {
+		System.out.print("ListNode ->  ");
+		while(temp!=null) {
+			System.out.print(temp.val+"-");
+			temp=temp.next;
+		}
+		System.out.print("\n");
+	}
+	
+	public static void main(String[] args) {
+		//init the list
+		ListNode root = init();
+		displayList(root);
+		ListNode tempRoot = root;
+
+		deleteDup(root);
+		displayList(root);
+	}
+	//remove duplicate from a listNode
+	public static void deleteDup(ListNode root) {
+		HashSet<Integer> set = new HashSet<>();
+		ListNode previous = new ListNode(0);
+		previous.next = root;
+		while(root!=null) {
+			if(set.contains(root.val)) {
+				previous.next=root.next;
+			}else {
+				set.add(root.val);
+				previous = root;
+			}
+			root=root.next;
+		}
+	}
+
 	
 	
 	
